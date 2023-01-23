2987229bc609dd48a9b73945d09ff41e47804972
sidahmed2306 <sidahmedbc1@gmail.com>
2023-01-23 10:40:42 +0100

    feat: add db connection and server setup

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,27 @@ const cors = require("cors");
 const morgan = require("morgan");
 const express = require("express");
 const { userRouter } = require("./routes");
+const { connectToDatabase } = require("./models/mongoose-setup");
 
-const PORT = process.env.PORT || 9001;
-const app = express();
+function configServer() {
+  const PORT = process.env.PORT || 9001;
+  const app = express();
 
-app.use(cors());
-app.use(morgan("dev"));
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(express.json());
 
-app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/stays", () => {});
-// app.use("/api/v1/chats", () => {});
+  app.use("/api/v1/users", userRouter);
+  // app.use("/api/v1/stays", () => {});
+  // app.use("/api/v1/chats", () => {});
 
-app.listen(PORT, () => console.log("Server listening on port", PORT));
+  return new Promise((resolve, _) => {
+    app.listen(PORT, () => {
+      console.log("Server listening on port", PORT);
+      resolve();
+    });
+  });
+}
+
+// Promise.all([connectToDatabase(), configServer()]);
+connectToDatabase().then(configServer);
