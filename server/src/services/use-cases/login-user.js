981c2e8d000e8de81b20b20d1c927514a91d2247
sidahmed2306 <sidahmedbc1@gmail.com>
2023-01-23 12:04:13 +0100

    feat: implement login service

--- server/src/services/use-cases/login-user.js
+++ server/src/services/use-cases/login-user.js
@@ -1,6 +1,25 @@
+const { User } = require("../../models");
+const { createHash } = require("../utilities/createHash");
+const { createToken } = require("../utilities/createToken");
+
 async function loginUser({ email, password }) {
-  console.log("inside loginUser service", { email, password });
-  return { accessToken: "", refreshToken: "" };
+  // validate if the user exists
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("user with this email does not exist");
+  }
+  // compare passowrd
+  const passwordhash = user.passwordHash;
+  const passwordSalt = user.passwordSalt;
+  const validPassword =
+    createHash(`${password}${passwordSalt}`) === passwordhash;
+  if (!validPassword) {
+    throw new Error("email or password false");
+  }
+  const accessToken = createToken(user, "access");
+  const refreshToken = createToken(user, "refresh");
+  return { accessToken, refreshToken };
 }
 
 module.exports = {
