8ac3762bafb71e52a9ad580b4b7431ab2f54b1ba
Ahmed Nasr <work@ahmednasr.at>
2023-01-25 11:59:47 +0100

    feat: implement register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
+import DashboardPage from "./pages/Dashboard";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Register/RegisterForm.js
+++ client/src/pages/Register/RegisterForm.js
@@ -1,7 +1,78 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { apiBaseUrl } from "../../api";
+
 const RegisterForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [bio, setBio] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  function register(event) {
+    event.preventDefault(); // page reload verhindern!
+
+    fetch(`${apiBaseUrl}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+        // FIXME: use multipart/form-data and upload the file as blob!!!
+        profilePicture,
+        bio,
+      }),
+    })
+      .then((res) => res.json())
+      .then(({ status, error }) => {
+        if (status === "error") {
+          // error handling...
+          setErrorMessage(error.message);
+          return;
+        }
+
+        return navigate("/dashboard");
+      });
+  }
+
   return (
     <form>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Tom"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Nasr"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="tom.nasr@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="hallo123"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={register}>Create Account</button>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </form>
   );
 };
