94d4acc53c843650661a17d8762f319619c711fb
Ahmed Nasr <work@ahmednasr.at>
2023-02-02 12:09:18 +0100

    feat: add validations (basic implementation)

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -2,11 +2,21 @@ const multer = require("multer");
 const express = require("express");
 const { userController } = require("../controllers");
 const { makeAuthMiddleware } = require("../middleware/doAuth");
+const { UserValidations } = require("../validations");
+const { validate } = require("../middleware/validate");
 
 const userRouter = express.Router();
 
-userRouter.post("/login", userController.postLogin);
-userRouter.post("/register", userController.postRegister);
+userRouter.post(
+  "/login",
+  validate(UserValidations.loginUserValidation.body),
+  userController.postLogin
+);
+userRouter.post(
+  "/register",
+  validate(UserValidations.registerUserValidation.body),
+  userController.postRegister
+);
 userRouter.post(
   "/refresh-token",
   makeAuthMiddleware({ tokenType: "refresh" }),
